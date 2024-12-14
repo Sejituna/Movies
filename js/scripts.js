6f2193943827bac0b33331efe4f0661a773952e8
Sejituna <mokhleselsayed7@gmail.com>
2024-12-14 10:47:37 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+function Movies(Movie){
+    img.src = Movie;;
+}
+function colors(color){
+    container.style.background = color; 
+}
